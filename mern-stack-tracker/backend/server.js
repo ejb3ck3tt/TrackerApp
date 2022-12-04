1c78e9a3e75b44372c880437df263c1433be6cc3
Ethel Beckett <joy@192-168-1-104.tpgi.com.au>
2022-12-04 16:16:45 +1000

    setup and connect to mongoDB database

--- mern-stack-tracker/backend/server.js
+++ mern-stack-tracker/backend/server.js
@@ -1,5 +1,9 @@
+//setup express
 const express = require('express')
+//setup middleware
 const cors = require('cors')
+//setup database
+const mongoose = require('mongoose')
 
 //for env variables
 require('dotenv').config()
@@ -8,10 +12,22 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
-//middleware
+//connect middleware
 app.use(cors())
 //allows to parse json for sending and receiving
 app.use(express.json())
+//database uri - refer to mongoDB to get the connection sring
+const URI = process.env.ATLAS_URI
+//pass the url where db is stored
+//start the connection
+mongoose.connect(URI, (err) => {
+  if (err) throw err
+})
+
+const connection = mongoose.connection
+connection.once('open', () => {
+  console.log('MongoDB database connection established  successfully')
+})
 
 //what starts the server
 app.listen(port, () => {
